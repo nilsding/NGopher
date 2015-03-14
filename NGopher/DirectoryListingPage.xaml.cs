c5a0e6592c3460fa0f4f0d5d9d688cee7594f82f
nilsding <nilsding@nilsding.org>
2015-03-14 01:32:32 +0100

    added DirectoryListingPage

---
+++ NGopher/DirectoryListingPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using NGopher.Gopher;
+
+namespace NGopher
+{
+    public sealed partial class DirectoryListingPage : Page
+    {
+        private GopherClient _gopher;
+        private string _selector = "/";
+
+        public DirectoryListingPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (e.Parameter == null) return;
+
+            var server = e.Parameter as string;
+            var x = server.Split(':');
+            var y = x[1].Split('|');
+            _gopher = new GopherClient
+            {
+                Server = x[0],
+                Port = Convert.ToUInt16(y[0])
+            };
+            _selector = y[1];
+            SelectorTextBlock.Text = _selector.ToUpper();
+            LoadDirectoryList();
+        }
+
+        private async void LoadDirectoryList()
+        {
+            var contents = await _gopher.GetDirectoryContents(_selector);
+            DirectoryListView.ItemsSource = contents;
+        }
+    }
+}
